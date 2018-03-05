3f76c8c08023343cae116da6e1fb77c0c6e4955d
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2018-03-05 11:11:27 +0800

    Create HelloPython.py

---
+++ HelloPython.py
@@ -0,0 +1,14 @@
+#/usr/bin/unicode
+#coding:utf-8
+greeting = "hello"
+name = "world"
+print(greeting,name)
+name = "python"
+print(greeting,name)
+say = "hh 0!! סחתיין עלייך(Sahteinaliha)!!"
+#say = say.encode('utf-8','strict')
+for countdown in 5,4,3,2,1,0.1,0.01,0.001,say:
+    print(countdown)
+hoo = "good good study day day up"
+foo = "good good study day day up"
+print(foo is hoo)
